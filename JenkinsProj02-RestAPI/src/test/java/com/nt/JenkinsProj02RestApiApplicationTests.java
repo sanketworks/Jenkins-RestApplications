43befe3f331732da34c12c213b70ab2013121a5b
sanket <mishra.sanket501@gmail.com>
2026-07-04 07:00:10 +0530

    initial commit

---
+++ JenkinsProj02-RestAPI/src/test/java/com/nt/JenkinsProj02RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsProj02RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
